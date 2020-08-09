3de35253cdffd03f683fcc6863f582004b706001
Willy <will@Willys-MacBook-Pro.local>
2020-08-09 16:31:11 +1000

    fix the stuff

--- main.js
+++ main.js
@@ -1,38 +1,49 @@
 const SCORE_EXPONENTIAL = 8;
 
-fetch('http://localhost:5000/api/team')
+// const HOST = 'http://localhost:5000';
+const HOST = 'https://ratings-2k-be-tumozo4dqa-ts.a.run.app';
+
+fetch(`${HOST}/api/team`)
     .then((response) => response.json())
     .then((response) => {
         const teamNames = Object.keys(response);
-        teamNames.forEach((teamName) => response[teamName].winEfficiency = response[teamName].wins / response[teamName].rating19_20);
+        teamNames.forEach((teamName) => response[teamName].winEfficiency = response[teamName].wins / response[teamName].expectedQuality19_20);
         teamNames.sort((a, b) => response[b].winEfficiency - response[a].winEfficiency);
         scaleValues(response, 'winEfficiency');
-        scaleValues(response, 'rating19_20');
-        scaleValues(response, 'totalRating');
+        scaleValues(response, 'expectedQuality19_20');
+        scaleValues(response, 'rating2k');
         const table = document.getElementById('teamTable');
         const tableBody = table.getElementsByTagName('tbody')[0];
         for (teamName of teamNames) {
             const newRow = tableBody.insertRow(tableBody.rows.length);
             const teamCell = newRow.insertCell(0);
             const winEfficiencyCell = newRow.insertCell(1);
-            const rating19_20 = newRow.insertCell(2);
+            const expectedQuality19_20 = newRow.insertCell(2);
             const winsCell = newRow.insertCell(3);
-            const totalRatingCell = newRow.insertCell(4);
+            const rating2kCell = newRow.insertCell(4);
             teamCell.appendChild(document.createTextNode(teamName));
             winEfficiencyCell.appendChild(document.createTextNode(response[teamName].winEfficiency));
-            rating19_20.appendChild(document.createTextNode(response[teamName].rating19_20));
+            expectedQuality19_20.appendChild(document.createTextNode(response[teamName].expectedQuality19_20));
             winsCell.appendChild(document.createTextNode(response[teamName].wins));
-            totalRatingCell.appendChild(document.createTextNode(response[teamName].totalRating));
+            rating2kCell.appendChild(document.createTextNode(response[teamName].rating2k));
         }
         table.style.display = 'table';
         document.getElementById('loading').style.display = 'none';
     });
 
-fetch('http://localhost:5000/api/player')
+fetch(`${HOST}/api/player`)
     .then((response) => response.json())
     .then((response) => {
         const playerNames = Object.keys(response);
-        playerNames.forEach((playerName) => response[playerName].value19_20 = (response[playerName].rating ** SCORE_EXPONENTIAL) * response[playerName].minutesPlayed);
+        playerNames.forEach((playerName) => {
+            teamMinutesCurrentTeam = response[playerName].teamsMinutes.find(({ teamName }) => teamName === response[playerName].teamName);
+            if (teamMinutesCurrentTeam !== undefined) {
+                response[playerName].minutesPlayed = teamMinutesCurrentTeam.minutes;
+            } else {
+                response[playerName].minutesPlayed = 0;
+            }
+            response[playerName].value19_20 = (response[playerName].rating ** SCORE_EXPONENTIAL) * response[playerName].minutesPlayed;
+        });
         playerNames.sort((a, b) => response[b].rating - response[a].rating);
         scaleValues(response, 'value19_20');
         const table = document.getElementById('playerTable');
